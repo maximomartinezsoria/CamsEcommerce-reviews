bde1e8df9ab1323e10cdd05d42edfa27c5e4e04b
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-05-18 21:07:54 -0300

    Initial commit

---
+++ src/main/java/com/mms/CamsEcommerceReviews/CamsEcommerceReviewsApplication.java
@@ -0,0 +1,13 @@
+package com.mms.CamsEcommerceReviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamsEcommerceReviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamsEcommerceReviewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mms/CamsEcommerceReviews/CamsEcommerceReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mms.CamsEcommerceReviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamsEcommerceReviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
