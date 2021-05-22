72b725962a8744578847df346e1f00e3d31bcd92
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-05-22 15:43:03 -0300

    Adds product repository

---
+++ src/test/java/com/mms/CamsEcommerceReviews/Product/ProductRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.mms.CamsEcommerceReviews.Product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+}
