e303cfbb33b4f1ba008b4329cd79731de546ee8e
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-05-24 22:11:05 -0300

    Adds user repository

---
+++ src/test/java/com/mms/CamsEcommerceReviews/User/UserRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.mms.CamsEcommerceReviews.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+}
